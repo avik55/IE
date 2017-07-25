d7b87de10dc06e5e7ab5c37ec95586cc9134ee9a
Avinash <avinash.khandekar08@gmail.com>
2017-07-25 12:26:19 +0530

    Email Service Correction

--- IndiaEntertainers/Models/EmailService.cs
+++ IndiaEntertainers/Models/EmailService.cs
@@ -56,27 +56,57 @@ namespace IndiaEntertainers.Models
         public bool SendByGmail(EmailMessageModel message)
         {
             try
-            {               
-                var smtpUserName = ConfigurationManager.AppSettings["smtpUserName"]; 
-                var smtpPassword = ConfigurationManager.AppSettings["smtpPassword"];
-                var smtpEmailFrom = ConfigurationManager.AppSettings["smtpEmailFrom"];
-                var smtpEmailBCC = ConfigurationManager.AppSettings["smtpEmailBCC"]; 
+            {
+                //var smtpUserName = ConfigurationManager.AppSettings["smtpUserName"]; 
+                //var smtpPassword = ConfigurationManager.AppSettings["smtpPassword"];
+                //var smtpEmailFrom = ConfigurationManager.AppSettings["smtpEmailFrom"];
+                //var smtpEmailBCC = ConfigurationManager.AppSettings["smtpEmailBCC"]; 
 
-                SmtpClient smtp = new SmtpClient();                
-                smtp.Credentials = new NetworkCredential(smtpUserName, smtpPassword);
-                smtp.Port = Convert.ToInt32(ConfigurationManager.AppSettings["smtpPort"]);
-                smtp.Host = ConfigurationManager.AppSettings["smtpHost"];
-                smtp.Timeout = Convert.ToInt32(ConfigurationManager.AppSettings["smtpTimeout"]);
-                smtp.EnableSsl = true;               
-                var message1 = new MailMessage();              
-                message1.From = new MailAddress(smtpEmailFrom);
+                //SmtpClient smtp = new SmtpClient();                
+                //smtp.Credentials = new NetworkCredential(smtpUserName, smtpPassword);
+                //smtp.Port = Convert.ToInt32(ConfigurationManager.AppSettings["smtpPort"]);
+                //smtp.Host = ConfigurationManager.AppSettings["smtpHost"];
+                //smtp.Timeout = Convert.ToInt32(ConfigurationManager.AppSettings["smtpTimeout"]);
+                //smtp.EnableSsl = true;               
+                //var message1 = new MailMessage();              
+                //message1.From = new MailAddress(smtpEmailFrom);
+                //message1.To.Add(new MailAddress(message.Destination));
+                //message1.Bcc.Add(new MailAddress(smtpEmailBCC));             
+                //message1.Subject = message.Subject;
+                //message1.Body = message.Body;
+                //message1.IsBodyHtml = true;
+                ////smtp.Send(message1);
+                //return true;
+
+
+                var GmailHost = "relay-hosting.secureserver.net";
+                var GmailPort = 25; // Gmail can use ports 25, 465 & 587; but must be 25 for medium trust environment.
+
+                SmtpClient smtp = new SmtpClient();
+                //smtp.Credentials = new NetworkCredential("mokcrm@gmail.com", "India0091#");
+                //smtp.Credentials = new NetworkCredential("rajesh.kumar@cazaayan.com", "Password@123");
+                smtp.Credentials = new NetworkCredential("info@indiaentertainers.com", "India0091#");
+                smtp.Port = GmailPort;
+                smtp.Host = GmailHost;
+                smtp.Timeout = 30000;
+                smtp.EnableSsl = false;
+                //  smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //  smtp.UseDefaultCredentials = false;
+
+                var message1 = new MailMessage();
+                //message1.From = new MailAddress("mokcrm@gmail.com");
+                //message1.From = new MailAddress("rajesh.kumar@cazaayan.com");
+                message1.From = new MailAddress("info@indiaentertainers.com");
                 message1.To.Add(new MailAddress(message.Destination));
-                message1.Bcc.Add(new MailAddress(smtpEmailBCC));             
+                message1.Bcc.Add(new MailAddress("info@indiaentertainers.com"));
+                //message1.Bcc.Add(new MailAddress("info@indiaentertainers.com"));
+                // message1.CC.Add(new MailAddress("athar@mokshasolutions.com"));
                 message1.Subject = message.Subject;
                 message1.Body = message.Body;
                 message1.IsBodyHtml = true;
-                //smtp.Send(message1);
+                smtp.Send(message1);
                 return true;
+
             }
             catch (Exception)
             {
